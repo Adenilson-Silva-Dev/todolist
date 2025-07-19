c3d304c47b2bdaa40c2d5f9ddf35ccc6ada2b965
Adenilson Silva <adenilsonjavascript@gmail.com>
2025-07-19 13:57:01 -0300

    implementation of user persistence with AsyncStorage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,12 +1,23 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
+useEffect(()=>{
+  async function loadStorange(){
+    const userStorange = await AsyncStorage.getItem('@mytask');
+    if(userStorange){
+      setUser(JSON.parse(userStorange))
+    }
+}
+
+loadStorange()
+},[])
   async function signUp(email, password, name) {
     try {
       await auth()
@@ -28,6 +39,7 @@ function AuthProvider({ children }) {
               };
 
               setUser(data);
+              storangeUser(data)
             });
         })
         .catch((err) => {
@@ -53,10 +65,15 @@ function AuthProvider({ children }) {
           };
 
           setUser(data);
+          storangeUser(data)
         });
     } catch (err) {
       console.log(err);
     }
+  }
+
+  async function storangeUser(data){
+    await AsyncStorage.setItem('@mytask', JSON.stringify(data))
   }
   return (
     <AuthContext.Provider value={{ signed: !!user, user, signUp,sigIn }}>{children}</AuthContext.Provider>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,33 @@
-import { Text, View,StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 
 function Profile() {
   return (
     <View style={styles.Container}>
-      <Text>Tela Profile</Text>
+      <TouchableOpacity style={styles.Button} activeOpacity={.8}>
+        <Text style={styles.TextButton}>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  Container:{
-    flex:1,
-    backgroundColor:'#1A1A2E'
+  Container: {
+    flex: 1,
+    alignItems:'center',
+    backgroundColor: '#1A1A2E',
+  },
+
+  Button:{
+    width:'80%',
+    height:50,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderColor:'#9B5DE5',
+    borderRadius:4
+  },
+  TextButton:{
+    color:'#fff',
+    fontSize:20
   }
-})
+});
 export default Profile;
